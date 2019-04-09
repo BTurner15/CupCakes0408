948a7cfb5bc882daae068018dd34bc34e5f57a1a
Bruce Turner <you@example.com>
2019-04-08 23:56:47 -0400

    Organizational Attempt with includes/functions.

---
+++ includes/functions.php
@@ -0,0 +1,23 @@
+<?php
+# functions.php
+/* Bruce Turner, IT 328, S2019 -->
+   https://bturner.greenriverdev.com/328/CupCakes0408/includes/functions.php -->
+   04-08-19 Rev.2
+*/
+//------------------------------------------------------------------------------
+//                          my_error_handler()
+// Create the error handler:
+function my_error_handler($e_number, $e_message, $e_file, $e_line, $e_vars) {
+    // Build the error message:
+    $message = "An error occurred in script '$e_file' on line $e_line: $e_message\n";
+    // Append $e_vars to  $message:
+    $message .= print_r ($e_vars, 1);
+    if (!LIVE) { // Development (print the error).
+        echo '<pre>' . $message . "\n";
+        debug_print_backtrace();
+        echo '</pre><br>';
+    } else { // Don't show the error.
+        echo '<div class="error">A system error occurred. We apologize for the inconvenience.</div><br>';
+    }
+}// End of my_error_handler() definition.
+//------------------------------------------------------------------------------
